72346d744ca988c168577ec99820a41e26062329
Grant <grg011@latech.edu>
2021-04-05 15:59:54 -0500

    Made starting classes

--- OptimizedGraphingTool.py
+++ OptimizedGraphingTool.py
@@ -1,3 +1,59 @@
+#############################################################################
+
+# main class
+class Graph:
+
+    # initiate every graph with origin of 0 by default
+    def __init__(self,origin=0):
+        self.origin = origin
+        
 
 
-print("test")
+    # getter and setter for origin
+    @property
+    def origin(self):
+        return self._origin
+
+    @origin.setter
+    def origin(self,origin):
+        self._origin = origin
+        
+
+  
+#############################################################################
+
+
+
+# Line and Circle classes inherit from Graph class
+class Line(Graph):
+
+    def __init__(self,origin,slope):
+        self.slope = slope
+        Graph.__init__(self,origin)
+
+
+   
+class Circle(Graph):
+
+    def __init__(self,origin,radius):
+        self.radius = radius
+        Graph.__init__(self,origin)
+
+
+
+# testing
+
+c = Circle(34,3)
+
+l = Line(8,2)
+
+print("circle origin = ",c.origin)
+
+print("circle radius = ",c.radius)
+
+print("slope of line = ",l.slope)
+
+
+
+
+################################################################################
