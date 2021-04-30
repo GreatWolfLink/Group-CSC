249d95c923222390cdc43e7397fc24167da9168a
Grant <grg011@latech.edu>
2021-04-29 22:54:11 -0500

    Fized up GUI

--- HomeScreen.py
+++ HomeScreen.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 import Plot as pl
+import sys 
 
+
+
+Font = ("Times",40)
 
 
 class GUI(tk.Tk):
@@ -17,7 +21,7 @@ class GUI(tk.Tk):
 
         self.frames = {}
 
-        for F in (StartPage, PageOne, PageTwo):
+        for F in (StartPage, PageOne, PageTwo, PageThree):
 
             frame = F(window,self)
 
@@ -34,21 +38,32 @@ class GUI(tk.Tk):
         frame.tkraise()
 
 
+  
 
-
-
+        
 class StartPage(tk.Frame):
 
     def __init__(self,parent,controller):
         tk.Frame. __init__(self,parent)
 
-        label = tk.Label(self,text="Optimized Graphing Tool")
+        label = tk.Label(self,text="Optimized Graphing Tool",font=Font)
         label.pack(pady=10,padx=10)
-        button1 = tk.Button(self,text="Plot",fg="red",command=lambda: controller.show_frame(PageOne))
+
+
+        button1 = tk.Button(self,text="Plot",fg="red",command=lambda: controller.show_frame(PageOne),padx=350,pady=100)
         button1.pack()
 
-        button2 = tk.Button(self,text="Options",fg="green",command=lambda: controller.show_frame(PageTwo))
+
+        button2 = tk.Button(self,text="Insctructions",fg="green",command=lambda: controller.show_frame(PageTwo),padx=350,pady=100)
         button2.pack()
+
+        
+        button3 = tk.Button(self,text="Creators",fg="orange",command=lambda: controller.show_frame(PageThree),padx=350,pady=100)
+        button3.pack()
+
+
+        button4 = tk.Button(self,text="Exit",fg="pink",padx=350,pady=100)
+        button4.pack()
 
         
 
@@ -57,13 +72,12 @@ class PageOne(tk.Frame):
     def __init__(self,parent,controller):
         tk.Frame. __init__(self,parent)
 
-        label = tk.Label(self,text="Plot")
+        label = tk.Label(self,text="Plot",font=Font)
         label.pack()
 
-        button = tk.Button(self,text="back",fg="blue", command=lambda: controller.show_frame(StartPage))
-        button.pack()
+        button = tk.Button(self,text="back",fg="blue", command=lambda: controller.show_frame(StartPage),padx=100,pady=25)
+        button.pack(side = tk.BOTTOM)
     
-
 
 class PageTwo(tk.Frame):
 
@@ -71,12 +85,29 @@ class PageTwo(tk.Frame):
         tk.Frame. __init__(self,parent)
 
 
-        label = tk.Label(self,text="Options")
+        label = tk.Label(self,text="Instructions",font=Font)
         label.pack()
 
-        button = tk.Button(self,text="back",fg="blue", command=lambda: controller.show_frame(StartPage))
-        button.pack()
+        button = tk.Button(self,text="back",fg="blue", command=lambda: controller.show_frame(StartPage),padx=100,pady=25)
+        button.pack(side = tk.BOTTOM)
    
+
+class PageThree(tk.Frame):
+
+    def __init__(self,parent,controller):
+        tk.Frame. __init__(self,parent)
+
+
+        label = tk.Label(self,text="Creators:",font=Font)
+        label.pack()
+
+        label = tk.Label(self,text="Grant Gremillion \n Connor Broussard \n Ethan Joyce",fg="green",font=Font)
+        label.pack()
+
+        button = tk.Button(self,text="back",fg="blue",command=lambda: controller.show_frame(StartPage),padx=100,pady=25)
+        button.pack(side = tk.BOTTOM)
+
+
 
 
 
