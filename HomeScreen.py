049e43b771d1ad90b9ee9f2629a2ff2654422797
ZappyPappy <56985293+ZappyPappy@users.noreply.github.com>
2021-05-05 15:07:09 -0500

    fixed button spacing, minor text fixes

--- HomeScreen.py
+++ HomeScreen.py
@@ -3,34 +3,28 @@ import tkinter.simpledialog as sd
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
-from PIL import ImageTk,Image
+from PIL import ImageTk, Image
 
-
-Font = ("Bahnschrift SemiCondensed",60)
-Font2 = ("Times",30)
+Font = ("Bahnschrift SemiCondensed", 60)
+Font2 = ("Times", 30)
 window = tk.Tk()
 frame = tk.Frame(window)
 window.state('zoomed')
-window.resizable(0,0)
+window.resizable(0, 0)
 WindowWidth = window.winfo_screenwidth()
 WindowHeight = window.winfo_screenheight()
 
 
-
-
-
 class GUI():
-    def __init__(self,frame,window):
-        
+    def __init__(self, frame, window):
         frame.pack(side="top", fill="both", expand=True)
         frame.grid_rowconfigure(0, weight=1)
         frame.grid_columnconfigure(0, weight=1)
-        
+
         self.frames = {}
 
         for F in (StartPage, PageOne, PageTwo, PageThree):
-
-            LocalFrame = F(frame,self)
+            LocalFrame = F(frame, self)
 
             self.frames[F] = LocalFrame
 
@@ -38,82 +32,68 @@ class GUI():
 
         self.show_frame(StartPage)
 
-
-    def show_frame(self,Page):
-
+    def show_frame(self, Page):
         frame = self.frames[Page]
         frame.tkraise()
-
 
     def Quit():
         window.destroy()
 
 
-
-
 class StartPage(tk.Frame):
 
-    def __init__(self,parent,controller):
-        tk.Frame. __init__(self,parent)
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
 
-    
-     
-        ButtonSpacingWeight = .04
+        ButtonSpacingWeight = .02
         ButtonWidthWeight = .2
-        
+
         spacing = int(WindowHeight * ButtonSpacingWeight)
         width = int(WindowWidth * ButtonWidthWeight)
-
 
         ButtonHeightWeight = 2
         FontColor = "blue"
 
-   
+        label = tk.Label(self, text="Optimized Graphing Tool", font=Font)
+        label.pack(fill=tk.BOTH, pady=spacing)
 
-        label = tk.Label(self,text="Optimized Graphing Tool",font=Font)
-        label.pack(fill=tk.BOTH,pady=spacing)
+        button1 = tk.Button(self, text="Plot", fg=FontColor, command=lambda: controller.show_frame(PageOne), font=Font2,
+                            height=ButtonHeightWeight)
+        button1.pack(fill=tk.BOTH, pady=spacing, padx=width)
 
+        button2 = tk.Button(self, text="Instructions", fg=FontColor, command=lambda: controller.show_frame(PageTwo),
+                            font=Font2, height=ButtonHeightWeight)
+        button2.pack(fill=tk.BOTH, pady=spacing, padx=width)
 
-      
-        button1 = tk.Button(self,text="Plot",fg=FontColor,command=lambda: controller.show_frame(PageOne),font=Font2,height=ButtonHeightWeight)
-        button1.pack(fill=tk.BOTH,pady=spacing,padx=width)
+        button3 = tk.Button(self, text="Creators", fg=FontColor, command=lambda: controller.show_frame(PageThree),
+                            font=Font2, height=ButtonHeightWeight)
+        button3.pack(fill=tk.BOTH, pady=spacing, padx=width)
 
+        button4 = tk.Button(self, text="Exit", fg=FontColor, command=lambda: GUI.Quit(), font=Font2,
+                            height=ButtonHeightWeight)
+        button4.pack(fill=tk.BOTH, pady=spacing, padx=width)
 
-        button2 = tk.Button(self,text="Instructions",fg=FontColor,command=lambda: controller.show_frame(PageTwo),font=Font2,height=ButtonHeightWeight)
-        button2.pack(fill=tk.BOTH,pady=spacing,padx=width)
-
-
-        
-        
-        button3 = tk.Button(self,text="Creators",fg=FontColor,command=lambda: controller.show_frame(PageThree),font=Font2,height=ButtonHeightWeight)
-        button3.pack(fill=tk.BOTH,pady=spacing,padx=width)
-
-
-        button4 = tk.Button(self,text="Exit",fg=FontColor,command=lambda: GUI.Quit(),font=Font2,height=ButtonHeightWeight)
-        button4.pack(fill=tk.BOTH,pady=spacing,padx=width)
-
-        
 
 class PageOne(tk.Frame):
 
-    def __init__(self,parent,controller):
-        tk.Frame. __init__(self,parent)
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
 
         self.can = None
         x = np.arange(-100, 100, 1)
-        label = tk.Label(self,text="Plot",font=Font)
+        label = tk.Label(self, text="Plot", font=Font)
         label.pack()
 
-        button = tk.Button(self,text="back",fg="blue",
-                           command=lambda: controller.show_frame(StartPage),padx=100,pady=25)
-        button.pack(side = tk.BOTTOM)
+        button = tk.Button(self, text="Back", fg="blue",
+                           command=lambda: controller.show_frame(StartPage), padx=100, pady=25)
+        button.pack(side=tk.BOTTOM)
 
         parabolaButton = tk.Button(self, text="Quadratic", fg="blue",
-                                   command=lambda: PageOne.ParabolaInput(self),padx=100,pady=25)
+                                   command=lambda: PageOne.ParabolaInput(self), padx=100, pady=25)
         parabolaButton.pack()
 
         LinearButton = tk.Button(self, text="Linear", fg="blue",
-                                   command=lambda: [PageOne.LinearInput(self)], padx=100, pady=25)
+                                 command=lambda: [PageOne.LinearInput(self)], padx=100, pady=25)
         LinearButton.pack()
 
     # Graphs based on an x value and a y value
@@ -139,57 +119,55 @@ class PageOne(tk.Frame):
         self.can.pack()
         print("hahahaha")
 
-    # Takes user input for the necessary intergers and uses them in an equation
+    # Takes user input for the necessary integers and uses them in an equation
     def ParabolaInput(self):
         x = np.arange(-100, 100, 1)
         a = sd.askfloat('User Input', "Input an A value")
         b = sd.askfloat('User Input', "Input a B value")
         c = sd.askfloat('User Input', "Input a C value")
-        y = (a*(x ** 2)) + (b * x) + c
+        y = (a * (x ** 2)) + (b * x) + c
         PageOne.graph(self, x, y)
 
     def LinearInput(self):
         x = np.arange(-100, 100, 1)
         m = sd.askfloat('User Input', "Input a slope")
         b = sd.askfloat('User Input', "Input a y-intercept")
-        y = (m*x)+b
+        y = (m * x) + b
         PageOne.graph(self, x, y)
+
 
 class PageTwo(tk.Frame):
 
-    def __init__(self,parent,controller):
-        tk.Frame. __init__(self,parent)
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
 
-
-        label = tk.Label(self,text="Instructions:",font=Font)
+        label = tk.Label(self, text="Instructions:", font=Font)
         label.pack()
 
-        label = tk.Label(self,text="  ",fg="green",font=Font2)
+        label = tk.Label(self, text="  ", fg="green", font=Font2)
         label.pack()
 
-        button = tk.Button(self,text="back",fg="blue", command=lambda: controller.show_frame(StartPage),padx=100,pady=25)
-        button.pack(side = tk.BOTTOM)
-   
+        button = tk.Button(self, text="back", fg="blue", command=lambda: controller.show_frame(StartPage), padx=100,
+                           pady=25)
+        button.pack(side=tk.BOTTOM)
+
 
 class PageThree(tk.Frame):
 
-    def __init__(self,parent,controller):
-        tk.Frame. __init__(self,parent)
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
 
-
-        label = tk.Label(self,text="Creators:",font=Font)
+        label = tk.Label(self, text="Creators:", font=Font)
         label.pack()
 
-        label = tk.Label(self,text="\nGrant Gremillion \n \n \nConnor Broussard \n \n \nEthan Joyce",fg="green",font=Font2)
+        label = tk.Label(self, text="\nGrant Gremillion \n \n \nConnor Broussard \n \n \nEthan Joyce", fg="green",
+                         font=Font2)
         label.pack()
 
-        button = tk.Button(self,text="back",fg="blue",command=lambda: controller.show_frame(StartPage),padx=100,pady=25)
-        button.pack(side = tk.BOTTOM)
+        button = tk.Button(self, text="back", fg="blue", command=lambda: controller.show_frame(StartPage), padx=100,
+                           pady=25)
+        button.pack(side=tk.BOTTOM)
 
 
-Menu = GUI(frame,window)
+Menu = GUI(frame, window)
 window.mainloop()
-
-
-
-
